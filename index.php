6e254e00beae6b00e2beae4a6734edd8885d4396
Pål Gjerde <paalvaa@gmail.com>
2014-09-17 11:11:29 +0200

    La til skjelettfiler for index.php og pepperkake.js.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pepperkake Adventures</title>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="pepperkake.js"></script>
+    </head>
+    <body>
+        <canvas id="game-window" width="800" height="600" style="border: 1px solid black;"></canvas>
+    </body>
+</html>
