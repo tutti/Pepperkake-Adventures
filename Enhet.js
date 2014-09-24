7ba6bc262984ee01efbb8f653c411ee037ea4f19
Pål Gjerde <paalvaa@gmail.com>
2014-09-24 08:23:07 +0200

    La til en nesten tom index.php-fil under presentasjonsmappen.

--- Enhet.js
+++ Enhet.js
@@ -1,3 +1,21 @@
-Enhet = function() {
+Enhet = function(bilde) {
+    this.bilde = bilde;
     
+    this.plattform = null;
+}
+
+Enhet.prototype.hent_element = function() {
+    // Hvis enheten har et HTML-element, returneres det.
+    // Ellers lages et nytt som returneres.
+    // Elementet skal uansett være et jQuery-objekt.
+    if (this.element === undefined) {
+        this.element = $('<img class="enhet enhet-' + this.type + '" src="' + this.bilde + '" />');
+        this.element.width(this.bredde);
+        this.element.height(this.hoyde);
+        this.element.css('left', this.x);
+        this.element.css('top', this.y);
+        $("#spillvindu").append(this.element);
+        this.element.hide();
+    }
+    return this.element;
 }
