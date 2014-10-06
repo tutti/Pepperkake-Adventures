4a6d037a11c8fff3b568df3cbfa04f157a715873
Pål Gjerde <paalvaa@gmail.com>
2014-10-06 13:33:51 +0200

    Fikk til landing på plattform, gå av plattform.

--- Spill.js
+++ Spill.js
@@ -1,3 +1,16 @@
 Spill = {
-    gravitasjon: 2
+    gravitasjon: 2,
+    brett: null
+}
+
+// Last inn data om brettene
+brett = [];
+for (b_id in brettdata) {
+    var data = JSON.parse(atob(brettdata[b_id]));
+    brett[b_id] = new Brett(data);
+}
+
+Spill.last_brett = function(brett_id) {
+    this.brett = brett[brett_id];
+    this.brett.last();
 }
