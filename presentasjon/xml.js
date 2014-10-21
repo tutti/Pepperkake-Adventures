337abe60bc54e75347742c2d2e293078062a8d3e
Pål Gjerde <paalvaa@gmail.com>
2014-10-21 09:57:46 +0200

    Forbedret våre personlige sider

--- presentasjon/xml.js
+++ presentasjon/xml.js
@@ -8,9 +8,10 @@
     
     var person = getParam("person");
     var bilder = [];
-    var type = "tekst";
     var src = "";
+    var type = "tekst";
     var tekst = "";
+    var alt = ""
     
     $(document).ready(function() {
         function callback(data) {
@@ -18,27 +19,38 @@
             $(data).find("BILDE").each(function() {
                 var bilde = {
                     'src': $(this).attr('src'),
+                    'alt': $(this).attr('alt'),
+                    'overskrift': $(this).find("OVERSKRIFT")[0].textContent,
                     'tekst': $(this).find("TEKST")[0].textContent,
                     'teksttype': $($(this).find("TEKST")[0]).attr('type')
                 };
                 bilder.push(bilde);
                 
-                var elmt = $('<img src="bilder/'+person+'/'+bilde.src+'" class="slidebilde" id="slide-'+(bilder.length-1)+'" slide="'+(bilder.length-1)+'" />');
-                $("#bildeslide-indre").append(elmt);
+                var ytre = $('<div class="slidebilde-wrapper">');
+                var elmt = $('<img src="bilder/'+person+'/'+bilde.src+'" class="slidebilde" id="slide-'+(bilder.length-1)+'" alt="'+bilde.alt+'" data-slide="'+(bilder.length-1)+'" />');
+                var overskrift = $('<span class="slide-overskrift">'+bilde.overskrift+'</span>');
+                bilde.element = elmt;
+                ytre.append(elmt);
+                ytre.append(overskrift);
+                $("#bildeslide-indre").append(ytre);
             });
             
             $("#bildeslide-indre").append('<div class="clearfloat">');
             
             src = bilder[0].src;
+            alt = bilder[0].alt;
+            overskrift = bilder[0].overskrift;
             type = bilder[0].teksttype;
             tekst = bilder[0].tekst;
+            bilder[0].element.addClass("valgt");
             onTimeout();
         }
         
         $.get("xml/"+person+".xml", callback);
         
         function onTimeout() {
-            $("#bilde").prop("src", "bilder/"+person+"/"+src);
+            $("#bilde").prop("src", "bilder/"+person+"/"+src).prop("alt", alt);
+            $("#person-overskrift").text(overskrift);
             if (type == "html") {
                 $("#infotekst").html(tekst)
             } else {
@@ -47,11 +59,15 @@
         }
         
         $("#bildeslide").on("click", "img", function() {
-            var bilde = $(this).attr("slide");
+            var bilde = $(this).attr("data-slide");
+            $("#bildeslide img.valgt").removeClass("valgt");
+            $(this).addClass("valgt");
             src = bilder[bilde].src;
+            alt = bilder[bilde].alt;
+            overskrift = bilder[bilde].overskrift;
             type = bilder[bilde].teksttype;
             tekst = bilder[bilde].tekst;
-            $("#bilde, #infotekst").stop(true, true).fadeOut(750).fadeIn(750);
+            $("#info").stop(true, true).fadeOut(750).fadeIn(750);
             setTimeout(onTimeout, 750);
         });
     });
